06ee85004bf88e9222bfffd14f2de45829a982ac
Hector Espert <hectorespertpardo@gmail.com>
2022-01-18 16:49:16 +0100

    Add manifest file

--- custom_components/som_energia/__init__.py
+++ custom_components/som_energia/__init__.py
@@ -1,1 +1,11 @@
 """Som Energia integration."""
+
+from homeassistant.core import Config, HomeAssistant
+
+DOMAIN = "som-energia"
+
+
+async def async_setup(hass: HomeAssistant, config: Config) -> bool:
+    """Set up configured FMI."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
