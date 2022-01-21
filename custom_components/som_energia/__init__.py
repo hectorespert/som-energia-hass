c97fed7edf8999fcb3ec8c8676029da6771b604d
Hector Espert <hectorespertpardo@gmail.com>
2022-01-21 22:27:58 +0100

    Add async_setup_entry

--- custom_components/som_energia/__init__.py
+++ custom_components/som_energia/__init__.py
@@ -1,5 +1,5 @@
 """Som Energia integration."""
-
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 
 from .const import DOMAIN
@@ -8,4 +8,9 @@ from .sensor import ElectricityPriceSensor
 
 async def async_setup(hass: HomeAssistant, config: Config) -> bool:
     hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Som Energia from a config entry."""
     return True
--- custom_components/som_energia/config_flow.py
+++ custom_components/som_energia/config_flow.py
@@ -23,4 +23,4 @@ class SomEnergiaConfigFlow(ConfigFlow, domain=DOMAIN):
 
         await self.async_set_unique_id('som_energia_unique')
         self._abort_if_unique_id_configured()
-        return self.async_create_entry(title='SomEnergia', data=user_input)
+        return self.async_create_entry(title='Som Energia', data=user_input)
