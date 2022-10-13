e30e2d6d3ce72d7a7e4473546358cf87511f5318
Hector Espert <hectorespertpardo@gmail.com>
2022-10-13 22:34:53 +0200

    Update prices

--- custom_components/som_energia/price/__init__.py
+++ custom_components/som_energia/price/__init__.py
@@ -7,11 +7,11 @@ def price(current_datetime: datetime) -> float:
     timezone_datetime = current_datetime.astimezone(timezone('Europe/Madrid'))
     weekday = timezone_datetime.isoweekday()
     if weekday == 6 or weekday == 7:
-        return 0.318605
+        return 0.284556
     hour = timezone_datetime.hour
     if 0 <= hour < 8:
-        return 0.318605
+        return 0.284556
     elif 8 <= hour < 10 or 14 <= hour < 18 or 22 <= hour < 24:
-        return 0.389136
+        return 0.341710
     else:
-        return 0.494932
+        return 0.417105
--- tests/test___init__.py
+++ tests/test___init__.py
@@ -5,15 +5,15 @@ from custom_components.som_energia.price import price
 
 def test_price_on_monday():
     monday = datetime(2022, 1, 24, 0, 0, 0)
-    assert price(monday) == 0.318605
+    assert price(monday) == 0.284556
 
     monday = datetime(2022, 1, 24, 8, 0, 0)
-    assert price(monday) == 0.389136
+    assert price(monday) == 0.34171
 
     monday = datetime(2022, 1, 24, 10, 0, 0)
-    assert price(monday) == 0.494932
+    assert price(monday) == 0.417105
 
 
 def test_price_on_sunday():
     sunday = datetime(2022, 1, 23, 16, 57, 0)
-    assert price(sunday) == 0.318605
+    assert price(sunday) == 0.284556
