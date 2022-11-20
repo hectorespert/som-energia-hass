2308649a0642c3ab680a81a5f3560acd925cdf36
xkill <pablo.catalina@gmail.com>
2022-11-20 22:22:03 +0100

    Actualizar precios
    Fix #1
    
    Término de energía
    
    Período punta 0,343 €/kWh
    Período llano 0,281 €/kWh
    Período valle 0,234 €/kWh

--- custom_components/som_energia/price/__init__.py
+++ custom_components/som_energia/price/__init__.py
@@ -7,11 +7,11 @@ def price(current_datetime: datetime) -> float:
     timezone_datetime = current_datetime.astimezone(timezone('Europe/Madrid'))
     weekday = timezone_datetime.isoweekday()
     if weekday == 6 or weekday == 7:
-        return 0.284556
+        return 0.234
     hour = timezone_datetime.hour
     if 0 <= hour < 8:
-        return 0.284556
+        return 0.234
     elif 8 <= hour < 10 or 14 <= hour < 18 or 22 <= hour < 24:
-        return 0.341710
+        return 0.281
     else:
-        return 0.417105
+        return 0.343
