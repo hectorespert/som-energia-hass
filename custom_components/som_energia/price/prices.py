f29b5f29f69f256dc89f7fd65c4db9d9f2b2a672
Javier Barrio <jbarrio@jbarrio.com>
2024-12-11 11:00:37 +0100

    Added branch with prices for Generation kWh

--- custom_components/som_energia/price/prices.py
+++ custom_components/som_energia/price/prices.py
@@ -1,40 +1,40 @@
 prices_data = {
     ("2024-11-01", "2999-12-31"): {
-        "punta": 0.226,
-        "llano": 0.166,
-        "valle": 0.127,
+        "punta": 0.186,
+        "llano": 0.135,
+        "valle": 0.109,
         "compensation": 0.050
     },
     ("2024-04-01", "2024-10-31"): {
-        "punta": 0.212,
-        "llano": 0.154,
-        "valle": 0.118,
+        "punta": 0.186,
+        "llano": 0.135,
+        "valle": 0.109,
         "compensation": 0.060
     },
     ("2024-01-01", "2024-03-31"): {
-        "punta": 0.247,
-        "llano": 0.189,
-        "valle": 0.154,
+        "punta": 0.187,
+        "llano": 0.139,
+        "valle": 0.116,
         "compensation": 0.070
     },
     ("2023-05-01", "2023-12-31"): {
-        "punta": 0.295,
-        "llano": 0.237,
-        "valle": 0.199,
+        "punta": 0.197,
+        "llano": 0.150,
+        "valle": 0.126,
         "compensation": 0.130
     },
     ("2023-01-01", "2023-04-30"): {
-        "punta": 0.342,
-        "llano": 0.281,
-        "valle": 0.234,
+        "punta": 0.183,
+        "llano": 0.135,
+        "valle": 0.111,
         "compensation": 0.0
     }
     # TODO: Add 2022 periods
     ,
     ("2022-01-01", "2022-01-31"): {
-        "punta": 0.396,
-        "llano": 0.286,
-        "valle": 0.228,
+        "punta": 0.236,
+        "llano": 0.137,
+        "valle": 0.105,
         "compensation": 0.0
     }
 
